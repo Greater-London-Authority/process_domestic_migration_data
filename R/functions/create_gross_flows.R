0f37ef7aaee99149925878dd097362f0e3c03e7b
Ben Corr <bjcorr606@gmail.com>
2024-04-26 09:13:56 +0100

    update output file names. add any missing combinations when creating gross flows

--- R/functions/create_gross_flows.R
+++ R/functions/create_gross_flows.R
@@ -7,13 +7,15 @@ create_gross_flows <- function(od_flows, rounding = 2) {
     group_by(across(-any_of(c("gss_out", "value")))) %>%
     summarise(internal_in = sum(value), .groups = "drop") %>%
     rename(gss_code = gss_in) %>%
-    mutate(internal_in = round(internal_in, rounding))
+    mutate(internal_in = round(internal_in, rounding)) %>%
+    complete(gss_code, age, year, sex, fill = list(internal_in = 0))
 
   outflows <- od_flows %>%
     group_by(across(-any_of(c("gss_in", "value")))) %>%
     summarise(internal_out = sum(value), .groups = "drop") %>%
     rename(gss_code = gss_out) %>%
-    mutate(internal_out = round(internal_out, rounding))
+    mutate(internal_out = round(internal_out, rounding)) %>%
+    complete(gss_code, age, year, sex, fill = list(internal_out = 0))
 
   gross_flows <- full_join(inflows, outflows, by = NULL) %>%
     replace_na(list(internal_in = 0, internal_out = 0)) %>%
