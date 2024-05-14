4c5c3b64911b8ac1bf7cd6780e52acea96f97447
Ben Corr <bjcorr606@gmail.com>
2024-05-14 17:45:19 +0100

    fix filepath typos

--- R/5_create_region_and_gross_flow_outputs.R
+++ R/5_create_region_and_gross_flow_outputs.R
@@ -6,6 +6,7 @@ fpath <- list(lad_od_data = "data/processed/full_series_lad.rds",
               region_od_data = "data/processed/region_od_series.rds",
               ctry_od_data = "data/processed/ctry_od_series.rds",
               region_gross_flows = "data/processed/region_gross_flows.rds",
+              ctry_gross_flows = "data/processed/ctry_gross_flows.rds",
               ctry_region_gross_flows = "data/processed/ctry_region_gross_flows.rds",
               lookup_lad_rgn_ctry = "lookups/lookup_lad_rgn_ctry.rds",
               lookup_lad_ctry = "lookups/lookup_lad_ctry.rds",
@@ -31,7 +32,9 @@ saveRDS(ctry_od_data, fpath$ctry_od_data)
 
 ctry_gross_flows <- create_gross_flows(ctry_od_data, rounding = 1)
 
+saveRDS(ctry_gross_flows, fpath$ctry_gross_flows)
+
 ctry_region_gross_flows <- bind_rows(filter(ctry_gross_flows, gss_code == "E92000001"),
                                      region_gross_flows)
 
-saveRDS(ctry_gross_flows, fpath$ctry_region_gross_flows)
+saveRDS(ctry_region_gross_flows, fpath$ctry_region_gross_flows)
