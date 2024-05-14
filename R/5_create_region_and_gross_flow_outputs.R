5b4a7454543cc0a466edc1a26fa4133e7870039f
Ben Corr <bjcorr606@gmail.com>
2024-05-14 17:30:08 +0100

    produce country to country OD outputs. Include England in country/regional gross flow output

--- R/5_create_region_and_gross_flow_outputs.R
+++ R/5_create_region_and_gross_flow_outputs.R
@@ -4,8 +4,12 @@ source("R/functions/aggregate_od_to_region.R")
 fpath <- list(lad_od_data = "data/processed/full_series_lad.rds",
               lad_gross_flows = "data/processed/lad_gross_flows.rds",
               region_od_data = "data/processed/region_od_series.rds",
+              ctry_od_data = "data/processed/ctry_od_series.rds",
               region_gross_flows = "data/processed/region_gross_flows.rds",
-              lookup_lad_rgn_ctry = "lookups/lookup_lad_rgn_ctry.rds")
+              ctry_region_gross_flows = "data/processed/ctry_region_gross_flows.rds",
+              lookup_lad_rgn_ctry = "lookups/lookup_lad_rgn_ctry.rds",
+              lookup_lad_ctry = "lookups/lookup_lad_ctry.rds",
+              lookup_lad_rgn = "lookups/lookup_lad_rgn.rds")
 
 lad_od_data <- readRDS(fpath$lad_od_data)
 
@@ -20,3 +24,14 @@ saveRDS(region_od_data, fpath$region_od_data)
 region_gross_flows <- create_gross_flows(region_od_data, rounding = 1)
 
 saveRDS(region_gross_flows, fpath$region_gross_flows)
+
+ctry_od_data <- aggregate_od_to_region(lad_od_data, readRDS(fpath$lookup_lad_ctry))
+
+saveRDS(ctry_od_data, fpath$ctry_od_data)
+
+ctry_gross_flows <- create_gross_flows(ctry_od_data, rounding = 1)
+
+ctry_region_gross_flows <- bind_rows(filter(ctry_gross_flows, gss_code == "E92000001"),
+                                     region_gross_flows)
+
+saveRDS(ctry_gross_flows, fpath$ctry_region_gross_flows)
--- lookups/create_lad_ctry_lookup.R
+++ lookups/create_lad_ctry_lookup.R
@@ -1,11 +1,19 @@
 library(tidyverse)
 
-fpath <- list(raw_lookup_lad_ctry = "data/raw/Local_Authority_District_to_Country_(April_2021)_Lookup_in_the_United_Kingdom.csv",
-              lookup_lad_ctry = "data/intermediate/lookup_lad_ctry.rds")
+fpath <- list(raw_lookup_lad_ctry = "lookups/LAD21_CTRY21_UK_LU.csv",
+              lookup_lad_ctry = "lookups/lookup_lad_ctry.rds")
 
 # https://geoportal.statistics.gov.uk/datasets/ons::local-authority-district-to-country-april-2021-lookup-in-the-united-kingdom/explore
+
+lookup_scot_ni <- data.frame(gss_code = c("N92000002", "S92000003"),
+                             gss_name = c("Northern Ireland", "Scotland"),
+                             RGNCD = c("N92000002", "S92000003"),
+                             RGNNM = c("Northern Ireland", "Scotland")
+)
+
 
 read_csv(fpath$raw_lookup_lad_ctry) %>%
     select(gss_code = LAD21CD, gss_name = LAD21NM, RGNCD = CTRY21CD, RGNNM = CTRY21NM) %>%
     distinct() %>%
+  bind_rows(lookup_scot_ni) %>%
   saveRDS(fpath$lookup_lad_ctry)
