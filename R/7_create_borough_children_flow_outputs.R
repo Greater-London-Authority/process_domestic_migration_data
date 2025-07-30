2b7dbd421ce8954a09fb2bcb36128801f88fe909
Ben Corr <bjcorr606@gmail.com>
2025-07-30 10:55:49 +0100

    add script to produce outputs for local authority school place planners with flows of children

--- R/7_create_borough_children_flow_outputs.R
+++ R/7_create_borough_children_flow_outputs.R
@@ -177,26 +177,3 @@ in_out_net_flows_wide <- in_out_net_flows %>%
 saveRDS(in_out_net_flows, fpath$in_out_net_flows)
 write_csv(in_out_net_flows_wide, fpath$in_out_net_flows_csv)
 
-#
-# all_od_flows_wide <- all_od_flows %>%
-#   mutate(value = round(value, 0)) %>%
-#   pivot_wider(values_from = "value", names_from = "age",
-#               values_fill = 0)
-
-
-
-
-# saveRDS(inner_outer_london_od_data, fpath$lookup_lad_inner_outer_london)
-# saveRDS(region_od_data, fpath$region_od_data)
-# saveRDS(ctry_od_data, fpath$ctry_od_data)
-#
-# inner_outer_london_gross_flows <- create_gross_flows(inner_outer_london_od_data, rounding = 1) %>%
-#   filter(gss_code != "other")
-# region_gross_flows <- create_gross_flows(region_od_data, rounding = 1)
-# ctry_gross_flows <- create_gross_flows(ctry_od_data, rounding = 1)
-#
-# ctry_region_gross_flows <- bind_rows(filter(ctry_gross_flows, gss_code == "E92000001"),
-#                                      region_gross_flows,
-#                                      inner_outer_london_gross_flows)
-#
-# saveRDS(ctry_region_gross_flows, fpath$ctry_region_gross_flows)
