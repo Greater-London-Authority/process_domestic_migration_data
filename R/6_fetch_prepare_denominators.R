da477530461e9bbf3376656b10d0579733691426
Ben Corr <bjcorr606@gmail.com>
2025-10-15 12:20:45 +0100

    update link to population estimates series following release of 2024 MYE and revised 2022 and 2023 data

--- R/6_fetch_prepare_denominators.R
+++ R/6_fetch_prepare_denominators.R
@@ -4,7 +4,7 @@ library(arrow)
 
 fpath <- list(population_coc = "data/processed/population_coc.rds")
 
-url_population_coc <- "https://data.london.gov.uk/download/modelled-population-backseries/2b07a39b-ba63-403a-a3fc-5456518ca785/full_modelled_estimates_series_EW%282023_geog%29.rds"
+url_population_coc <- "https://data.london.gov.uk/download/fb203828-bde5-4a50-96d9-8adfb4960631/ba752f34-0b54-4184-9251-8e2e94ae97ee/full_modelled_estimates_series_EW(2023_geog).rds"
 
 
 if(!file.exists(fpath$population_coc)) {
